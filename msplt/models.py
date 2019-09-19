101a003dbdc27d86a12f6a0472ad77cbcf6e7c8f
HouSo1in <HouSo1in@users.noreply.github.com>
2019-09-19 15:57:19 +0800

    添加普通用户表

--- msplt/models.py
+++ msplt/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+
+# 普通用户表
+class UserProfile(models.Model):
+
+    gender = (
+        ('male', '男'),
+        ('female', '女'),
+    )
+
+    name = models.CharField(verbose_name='昵称', max_length=128, unique=True)
+    password = models.CharField(verbose_name='密码', max_length=16)
+    email = models.EmailField(verbose_name='电子邮箱', unique=True)
+    sex = models.CharField(verbose_name='性别', max_length=32, choices=gender, default='男')
+    create_time = models.DateTimeField(verbose_name='创建日期', auto_now_add=True)
+
+    class Meta:
+        ordering = ['create_time']
+        verbose_name = '用户'
+        verbose_name_plural = '用户'
+
+    def __str__(self):
+        return self.name
