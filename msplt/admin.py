c99632182e538f24785ea9842080605f03a0ea4a
HouSo1in <HouSo1in@users.noreply.github.com>
2019-09-19 21:41:58 +0800

    添加示例页面

--- msplt/admin.py
+++ msplt/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import UserProfile
 # Register your models here.
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'sex', 'create_time')
+    list_display_links = ('name',)
+
+
+admin.site.register(UserProfile, UserAdmin)
--- msplt/views.py
+++ msplt/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    pass
+    return render(request, 'index.html')
+
+
+def login(request):
+    pass
+    return render(request, 'login.html')
