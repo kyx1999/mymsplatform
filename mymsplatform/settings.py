3238733d0ef1f8e947c5e1e8d0bb384726459aad
HouSo1in <HouSo1in@users.noreply.github.com>
2019-09-20 14:00:06 +0800

    增加bootstrap模块

--- mymsplatform/settings.py
+++ mymsplatform/settings.py
@@ -120,6 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 # 文件上传目录， 用于储存上传的图片、文件等
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
