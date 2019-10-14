c80229b27f7dde8d1a905cf0701e9831803ffe42
LeyN <z1010078345@163.com>
2019-10-14 10:24:59 +0800

    undo revert *2

--- msplt/lib.py
+++ msplt/lib.py
@@ -4,7 +4,7 @@ from pprint import pprint
 from kubernetes import client, config, watch
 
 
-class lib(object):
+class manager(object):
     def __init__(self):
         config.load_kube_config('E:/pythonProject/test/resource/config')
         self.core_v1 = client.CoreV1Api()
